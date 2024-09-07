019ec8ab93f9a972c1eb87f7aa8a9e16507b71e5
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-09-07 09:19:36 +0545

    feat: repo init to test lexical serialization

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import theme from "./theme";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+const initialConfig = {
+  namespace: "MyEditor",
+  theme,
+  onError(error: Error) {
+    throw error;
+  },
+};
+
+function Editor() {
+  return (
+    <div>
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={
+            <div className="editor-scroller">
+              <div className="editor">
+                <ContentEditable
+                  aria-placeholder="Write something"
+                  placeholder={<span>Write something</span>}
+                />
+              </div>
+            </div>
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />{" "}
+        <HistoryPlugin />
+        <OnChangePlugin onChange={console.log} />
+      </LexicalComposer>
+    </div>
+  );
+}
+
+function Transformed() {
+  return <div>Transformed</div>;
+}
+
+function App() {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <Editor />
+      <Transformed />
+    </div>
+  );
+}
+
+export default App;
